c6ab08f103c50a6bdb28605499d84f12db331346
Braulio Lazo <yobraulio23@gmail.com>
2023-03-19 18:12:18 -0400

    NumbersToAnimate

--- JavaScript/startAnimations.js
+++ JavaScript/startAnimations.js
@@ -1,3 +1,5 @@
+let numberToAnimate;
+
 function startAnimations() {
     window.addEventListener("scroll", onScroll);
 }
@@ -11,10 +13,40 @@ function onScroll() {
             appearLine(element);
         }
     });
+
+    numberToAnimate = document.querySelectorAll(".number__to__animate");
+    numberToAnimate.forEach(function (element) {
+        let elementDistance = window.innerHeight - element.getBoundingClientRect().top;
+        if (elementDistance >= 100) {
+            if (parseInt(element.innerHTML) == 0) {
+                animateNumber(element);
+            }
+        }
+    });
 }
 
-function appearLine(element){
+function appearLine(element) {
     element.style.width = "100%";
     element.style.opacity = "1";
     element.style.transition = "all 1.5s ease-out";
+}
+
+function animateNumber(element) {
+    let counter = 0;
+    let elementValue = parseInt(element.getAttribute("data-value"));
+    const interval = setInterval(() => {
+        counter++;
+        if (element == numberToAnimate[0]) {
+            element.innerHTML = "+" + counter;
+            element.style.opacity = "1";
+            element.style.transition = "all 1.5s ease-out";
+        } else {
+            element.innerHTML = counter;
+            element.style.opacity = "1";
+            element.style.transition = "all 1.5s ease-out";
+        }
+        if (counter == elementValue) {
+            clearInterval(interval);
+        }
+    }, 2000 / elementValue);
 }
