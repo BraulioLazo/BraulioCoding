b6d79840ba02de1b1c7eae33440c5f4dd5a8940c
Braulio Lazo <yobraulio23@gmail.com>
2023-03-19 21:10:40 -0400

    Animation UP and animation RIGHT

--- JavaScript/startAnimations.js
+++ JavaScript/startAnimations.js
@@ -1,9 +1,21 @@
 let numberToAnimate;
 
 function startAnimations() {
+    animationRightOnload();
     window.addEventListener("scroll", onScroll);
+
 }
 window.addEventListener("load", startAnimations);
+
+
+function animationRightOnload(){
+    let animateToRightOnLoad = document.querySelectorAll(".animation__right__on__load");
+    animateToRightOnLoad.forEach(function (element) {
+        animationRight(element);
+    });
+}
+
+/* Animination on SCROLL -------------------------------*/
 
 function onScroll() {
     let linesToAnimate = document.querySelectorAll(".animation__line");
@@ -23,13 +35,35 @@ function onScroll() {
             }
         }
     });
+
+    let animateToRight = document.querySelectorAll(".animation__right");
+    animateToRight.forEach(function (element) {
+        let elementDistance = window.innerHeight - element.getBoundingClientRect().top;
+        if (elementDistance >= 100) {
+            animationRight(element);
+        }
+    });
+
+  
+    let animateToUp = document.querySelectorAll(".animation__up");
+    animateToUp.forEach(function (element) {
+        let elementDistance = window.innerHeight - element.getBoundingClientRect().top;
+        if (elementDistance >= 100) {
+            animationUp(element);
+        }
+    });
 }
+
+/*Animation for LINES -----------------------------------*/
 
 function appearLine(element) {
     element.style.width = "100%";
     element.style.opacity = "1";
     element.style.transition = "all 1.5s ease-out";
 }
+
+
+/*Animation for NUMBERS ---------------------------------*/
 
 function animateNumber(element) {
     let counter = 0;
@@ -44,9 +78,30 @@ function animateNumber(element) {
             element.innerHTML = counter;
             element.style.opacity = "1";
             element.style.transition = "all 1.5s ease-out";
-        }
-        if (counter == elementValue) {
+        } if (counter == elementValue) {
             clearInterval(interval);
         }
-    }, 2000 / elementValue);
+    }, 4000 / elementValue);
+}
+
+/* Function ANIMATION to move to RIGHT ------------------*/
+
+function animationRight(element) {
+    element.style.transform = "translateX(0px)";
+    element.style.opacity = "1";
+    element.style.transition = "all 1.5s ease-out";
+
+    let btnTransitionReset = document.querySelectorAll(".bc__btn");
+    setTimeout(() => {
+        btnTransitionReset.forEach(function (element) {
+            element.style.transition = "all .5s ease";
+        });
+    }, 1500);
+}
+
+
+function animationUp(element) {
+    element.style.transform = "translateY(0px)";
+    element.style.opacity = "1";
+    element.style.transition = "all 1.5s ease-out";
 }
