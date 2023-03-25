20c1153659c8056f2fb8e1544ed8ddfbf9fa5a5f
Braulio Lazo <yobraulio23@gmail.com>
2023-03-25 10:21:22 -0400

    Deploy USER INFO

--- Proyects/TaskKeeper/JavaScript/main.js
+++ Proyects/TaskKeeper/JavaScript/main.js
@@ -1,12 +1,18 @@
-function startTaskKeeper(){
+function startTaskKeeper() {
   deployMenu();
 }
 window.addEventListener("load", startTaskKeeper);
 
-function deployMenu(){
-    const menuIcon = document.querySelector(".tk__icono__menu__container");
-    menuIcon.addEventListener("click", () => {
-        const menu = document.querySelector(".tk__section_menu");
-        menu.classList.toggle("deploy__menu");
-    })
+function deployMenu() {
+  const menuIcon = document.querySelector(".tk__icono__menu__container");
+  menuIcon.addEventListener("click", () => {
+    const menu = document.querySelector(".tk__section_menu");
+    menu.classList.toggle("deploy__menu");
+  });
+
+  const iconUserInfo = document.querySelector(".tk__icono__user__info__container");
+  iconUserInfo.addEventListener("click", () => {
+    const sectionUserInfo = document.querySelector(".tk__section__user__info");
+    sectionUserInfo.classList.toggle("deploy__menu");
+  });
 }
