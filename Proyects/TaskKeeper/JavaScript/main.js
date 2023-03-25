c8ab6d2e77fc36446ba629d5610edca10fb5b9a7
Braulio Lazo <yobraulio23@gmail.com>
2023-03-25 12:17:27 -0400

    JavaScript CHANGES

--- Proyects/TaskKeeper/JavaScript/main.js
+++ Proyects/TaskKeeper/JavaScript/main.js
@@ -1,8 +1,40 @@
 function startTaskKeeper() {
   deployChangeName();
+  generateUserName();
+  changeUserName();
   deployMenu();
 }
 window.addEventListener("load", startTaskKeeper);
+
+
+
+function deployChangeName() {
+  const userLogo = document.querySelector(".tk__user__logo");
+  userLogo.addEventListener("click", () => {
+    const containerChangeName = document.querySelector(".container__input__change__name");
+    containerChangeName.classList.toggle("deploy__change__name");
+  });
+}
+
+function generateUserName() {
+  const containerUserName = document.querySelector(".tk__user__name__container");
+  if (localStorage.getItem("username")) {
+      const userName = localStorage.getItem("username");
+      containerUserName.innerHTML = "¡Hola " + userName + "!";
+  } else {
+      localStorage.setItem("username", "Usuario");
+      containerUserName.innerHTML = "¡Hola Usuario!";
+  }
+}
+
+function changeUserName(){
+  let formChageName = document.querySelector("#form__change__name");
+    formChageName.addEventListener("submit", () => {
+        const name = document.querySelector("#input__change__name").value;
+        localStorage.setItem("username", name);
+        deployChangeName();
+    });
+}
 
 function deployMenu() {
   const menuIcon = document.querySelector(".tk__icono__menu__container");
@@ -15,13 +47,5 @@ function deployMenu() {
   iconUserInfo.addEventListener("click", () => {
     const sectionUserInfo = document.querySelector(".tk__section__user__info");
     sectionUserInfo.classList.toggle("deploy__menu");
-  });
-}
-
-function deployChangeName() {
-  const userLogo = document.querySelector(".tk__user__logo");
-  userLogo.addEventListener("click", () => {
-    const containerChangeName = document.querySelector(".container__input__change__name");
-    containerChangeName.classList.toggle("deploy__change__name");
   });
 }
