73c98d38b6ffe4e04307ba10cc9af570e6b563b0
Braulio Lazo <yobraulio23@gmail.com>
2023-03-25 17:59:06 -0400

    Add NOTE ready

--- Proyects/TaskKeeper/JavaScript/DataBase.js
+++ Proyects/TaskKeeper/JavaScript/DataBase.js
@@ -36,3 +36,68 @@ function createStorage(event) {
 
     storageTask.createIndex("LookForPriority", "priority", { unique: false });
 }
+
+function addNote() {
+    generateKey();
+    ideasKey = localStorage.getItem("Task-Key");
+    const noteTitle = document.querySelector("#tk__input__title").value;
+    const note = document.querySelector("#tk__note__area").value;
+    const priority = document.querySelector("#tk__select__priority").value;
+
+    const dbTransaction = db.transaction(["Task-Keeper"], "readwrite");
+    const dbStorage = dbTransaction.objectStore("Task-Keeper");
+    dbTransaction.addEventListener("complete", beforeShowNotes);
+
+    const request = dbStorage.add({
+        key: ideasKey,
+        notetitle: noteTitle,
+        note: note,
+        priority: priority
+    });
+
+    document.querySelector("#tk__input__title").value = "";
+    document.querySelector("#tk__note__area").value = "";
+}
+
+function showNotes() {
+
+    boxNotes.innerHTML = "";
+    const dbTransaction = db.transaction(["Task-Keeper"]);
+    const dbStorage = dbTransaction.objectStore("Task-Keeper");
+    const pointer = dbStorage.openCursor();
+    pointer.addEventListener("success", (event) => {
+        const pointer = event.target.result;
+        if (pointer) {
+            boxNotes.innerHTML += '<div class="tk__note__container">' +
+                '<div class="tk__note__menu__container">' +
+                '<div class="tk__note__menu__image">' +
+                '<img src="images/note__menu__close__image.webp" alt="">' +
+                '</div>' +
+
+                '<div class="tk__note__menu__container__button__image">' +
+
+                '<button class="note__btn__edit" onclick="selectTask(\'' + pointer.value.key + '\')">' + 'Editar' + '</button>' +
+                '<button class="note__btn__delete" onclick="beforeDeleteTask(\'' + pointer.value.key + '\')">' + 'Eliminar' + '</button>' +
+                '</div>' +
+                '</div>' +
+
+                '<div class="tk__container__title">' +
+                pointer.value.notetitle +
+                '</div>' +
+                '<div class="tk__container__note">' +
+                pointer.value.note +
+                '</div>' +
+                '<div class="tk__note__pririty__done__container">' +
+                '<p class="tk__p__priority">' +
+                pointer.value.priority +
+                '</p>' +
+                '<button class="tk__work__done" onclick="deleteTaskAddPonits(\'' + pointer.value.key + '\')">' + 'Terminado' + '</button>' +
+                '</div>' +
+                '</div>';
+            pointer.continue();
+            deployNoteForm()
+        }
+        priorityColors();
+    });
+}
+
--- Proyects/TaskKeeper/JavaScript/main.js
+++ Proyects/TaskKeeper/JavaScript/main.js
@@ -9,6 +9,19 @@ function startTaskKeeper() {
   deployMenu();
   updateUserLevel();
   resetProgram();
+
+  const btnNewTask = document.querySelector("#tk__btn__new__note");
+  btnNewTask.addEventListener("click", () => {
+    deployNoteForm();
+  });
+
+  const btnCloseForm = document.querySelector("#tk__close__form");
+  btnCloseForm.addEventListener("click", () => {
+    deployNoteForm();
+  });
+
+  const btnKeepNote = document.querySelector("#btn__keepnote");
+  btnKeepNote.addEventListener("click", addNoteCloseForm);
 }
 window.addEventListener("load", startTaskKeeper);
 
@@ -25,21 +38,21 @@ function deployChangeName() {
 function generateUserName() {
   const containerUserName = document.querySelector(".tk__user__name__container");
   if (localStorage.getItem("username")) {
-      const userName = localStorage.getItem("username");
-      containerUserName.innerHTML = "¡Hola " + userName + "!";
+    const userName = localStorage.getItem("username");
+    containerUserName.innerHTML = "¡Hola " + userName + "!";
   } else {
-      localStorage.setItem("username", "Usuario");
-      containerUserName.innerHTML = "¡Hola Usuario!";
+    localStorage.setItem("username", "Usuario");
+    containerUserName.innerHTML = "¡Hola Usuario!";
   }
 }
 
-function changeUserName(){
+function changeUserName() {
   let formChageName = document.querySelector("#form__change__name");
-    formChageName.addEventListener("submit", () => {
-        const name = document.querySelector("#input__change__name").value;
-        localStorage.setItem("username", name);
-        deployChangeName();
-    });
+  formChageName.addEventListener("submit", () => {
+    const name = document.querySelector("#input__change__name").value;
+    localStorage.setItem("username", name);
+    deployChangeName();
+  });
 }
 
 function deployMenu() {
@@ -60,36 +73,69 @@ function updateUserLevel() {
   const levelContainer = document.querySelector(".tk__level__container");
   if (localStorage.getItem("taskDoneCounter")) {
 
-      const taskDone = parseInt(localStorage.getItem("taskDoneCounter"));
+    const taskDone = parseInt(localStorage.getItem("taskDoneCounter"));
 
-      if (taskDone < levelAdvanced) {
-          localStorage.setItem("UserLevel", "Amateur");
-          levelContainer.innerHTML = '<img src="images/amateur__crown__image.webp">' +
-              localStorage.getItem("UserLevel") +
-              '<img src="images/amateur__crown__image.webp">';
-      } else if (taskDone >= levelAdvanced && taskDone < levelPro) {
-          localStorage.setItem("UserLevel", "Advanced");
-          levelContainer.innerHTML = '<img src="images/advanced__crown__image.webp">' +
-              localStorage.getItem("UserLevel") +
-              '<img src="images/advanced__crown__image.webp">';
-      } else if (taskDone >= levelPro) {
-          localStorage.setItem("UserLevel", "Pro");
-          levelContainer.innerHTML = '<img src="images/pro__crown__image.webp">' +
-              localStorage.getItem("UserLevel") +
-              '<img src="images/pro__crown__image.webp">';
-      }
-  } else {
+    if (taskDone < levelAdvanced) {
       localStorage.setItem("UserLevel", "Amateur");
       levelContainer.innerHTML = '<img src="images/amateur__crown__image.webp">' +
-          localStorage.getItem("UserLevel") +
-          '<img src="images/amateur__crown__image.webp">';
+        localStorage.getItem("UserLevel") +
+        '<img src="images/amateur__crown__image.webp">';
+    } else if (taskDone >= levelAdvanced && taskDone < levelPro) {
+      localStorage.setItem("UserLevel", "Advanced");
+      levelContainer.innerHTML = '<img src="images/advanced__crown__image.webp">' +
+        localStorage.getItem("UserLevel") +
+        '<img src="images/advanced__crown__image.webp">';
+    } else if (taskDone >= levelPro) {
+      localStorage.setItem("UserLevel", "Pro");
+      levelContainer.innerHTML = '<img src="images/pro__crown__image.webp">' +
+        localStorage.getItem("UserLevel") +
+        '<img src="images/pro__crown__image.webp">';
+    }
+  } else {
+    localStorage.setItem("UserLevel", "Amateur");
+    levelContainer.innerHTML = '<img src="images/amateur__crown__image.webp">' +
+      localStorage.getItem("UserLevel") +
+      '<img src="images/amateur__crown__image.webp">';
   }
 
 }
 
-function resetProgram(){
+function resetProgram() {
   document.querySelector(".tk__tools").addEventListener("click", () => {
     localStorage.clear();
     location.reload();
-  })
+  });
+}
+
+function deployNoteForm() {
+  const sectionNoteForm = document.querySelector(".tk__section__note__form");
+  sectionNoteForm.classList.toggle("deploy__form");
+}
+
+function addNoteCloseForm() {
+  addNote();
+  deployNoteForm();
+}
+
+function beforeShowNotes() {
+  if (lastPrioritySearch === null) {
+      showNotes();
+  } else {
+      lastSearch();
+  }
+}
+
+function priorityColors() {
+  workPriority = document.querySelectorAll(".tk__p__priority");
+  workPriority.forEach(function (element) {
+      if (element.innerHTML === "Alta") {
+          element.style.backgroundColor = "#f22f7033";
+      } else if (element.innerHTML === "Media") {
+          element.style.backgroundColor = "rgba(1, 255, 1, 0.3)";
+          element.style.color = "rgba(1, 148, 1)";
+      } else if (element.innerHTML === "Baja") {
+          element.style.backgroundColor = "rgba(230, 255, 3, 0.3)";
+          element.style.color = "rgb(250, 171, 0)";
+      }
+  });
 }
